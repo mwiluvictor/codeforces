7a4603ffaf202415c99e6ffb5ec0b0a12530d5bc
mwiluvictor <mwilumuisyo@gmail.com>
2024-04-11 14:57:56 +0300

    updated sln to 1A

--- 1A Theatre Square.cpp
+++ 1A Theatre Square.cpp
@@ -3,21 +3,9 @@
 using namespace std;
 
 int main(){
-    int n, m, a;
+    long double n, m, a;
     cin >> n >> m >> a;
 
-    int x, y, s;
-    x = n/a;
-    y = m/a;
-
-    if(n%a >= 1){
-        x+=1;
-    }
-    if (m%a >= 1){
-        y+=1;
-    }
-
-    s = x*y;
-
-    cout << s << endl;
+    cout << (((n+a-1)/a)*((m+a-1)/a)) << endl;
+    return 0;
 }
